93ea5e5d9a86cf006d64a826a09e4cfd73c903d1
Asko Soukka <asko.soukka@iki.fi>
2011-10-26 19:15:28 +0300

    Created this separate egg for just the recipe from corejet.testrunner.

---
+++ setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+
+version = "1.0"
+
+setup(name="corejet.recipe.testrunner",
+      version=version,
+      description="ZC Buildout recipe for creating CoreJet test runners",
+      long_description=open("README.txt").read() + "\n" +
+                       open("CHANGES.txt").read(),
+      # Get more strings from
+      # http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+          "Programming Language :: Python",
+      ],
+      keywords="corejet recipe",
+      author="Asko Soukka",
+      author_email="asko.soukka@iki.fi",
+      url="https://github.com/datakurre/corejet.recipe.testrunner/",
+      license="GPL",
+      packages=find_packages(exclude=["ez_setup"]),
+      namespace_packages=["corejet", "corejet.recipe"],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          "setuptools",
+          "zc.buildout",
+          "zc.recipe.egg",
+      ],
+      entry_points="""
+      [zc.buildout]
+      default = corejet.recipe.testrunner:TestRunner
+      """,
+      )
